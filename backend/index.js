b611f05fc08f378b9635a948d4d27cf65254beff
premnewbie <prempm50@gmail.com>
2025-04-27 23:57:14 +0530

    Changes are made

--- backend/index.js
+++ backend/index.js
@@ -34,7 +34,7 @@ app.use("/api", simplifiedRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/dist")));
   
-  app.get("/*", function(req, res) {
+  app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
   });
 }
