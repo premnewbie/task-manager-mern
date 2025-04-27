971ae7f838c6b4f7f3e6d11552622f8e5594919a
premnewbie <prempm50@gmail.com>
2025-04-28 00:14:58 +0530

    added deployment url

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 
 if (process.env.NODE_ENV === "production") {
   app.use(cors({
-    origin: true,
+    origin: 'https://task-manager-mern-3v41.onrender.com',
     credentials: true,
   }));
 } else {
